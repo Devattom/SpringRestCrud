3a787df814624a2ee8d843cd5b8ee9bba46829a7
Rousselin Thomas <98778784+Devattom@users.noreply.github.com>
2025-05-25 11:36:17 +0200

    use jpaRepository (#7)

--- src/main/java/com/devattom/SpringRestCrud/service/StudentServiceImpl.java
+++ src/main/java/com/devattom/SpringRestCrud/service/StudentServiceImpl.java
@@ -1,41 +1,46 @@
 package com.devattom.SpringRestCrud.service;
 
-import com.devattom.SpringRestCrud.DAO.StudentDAO;
 import com.devattom.SpringRestCrud.entity.Student;
+import com.devattom.SpringRestCrud.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class StudentServiceImpl  implements StudentService {
-    private StudentDAO studentDAO;
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
 
     @Autowired
-    public StudentServiceImpl(StudentDAO studentDAO) {
-        this.studentDAO = studentDAO;
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
 
     @Override
     public List<Student> findAll() {
-        return studentDAO.findAll();
+        return studentRepository.findAll();
     }
 
     @Override
     public Student findById(int id) {
-        return studentDAO.findById(id);
+        Optional<Student> student = studentRepository.findById(id);
+
+        if(student.isEmpty()) {
+            throw new RuntimeException("Did not find student id - " + id);
+        }
+
+        return student.get();
     }
 
     @Override
-    @Transactional
     public Student save(Student student) {
-        return studentDAO.save(student);
+        return studentRepository.save(student);
     }
 
     @Override
-    @Transactional
     public void deleteById(int id) {
-        studentDAO.deleteById(id);
+        studentRepository.deleteById(id);
     }
 }
