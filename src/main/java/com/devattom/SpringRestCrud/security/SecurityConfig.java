750adccacb6e58af4afa3b05dc072ce5c9d5adf4
Rousselin <devattom@gmail.com>
2025-05-27 19:02:12 +0200

    add role restrictions

--- src/main/java/com/devattom/SpringRestCrud/security/SecurityConfig.java
+++ src/main/java/com/devattom/SpringRestCrud/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.devattom.SpringRestCrud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -32,5 +37,24 @@ public class SecurityConfig {
 
 
         return new InMemoryUserDetailsManager(toto, luz, david);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests(configurer->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/students").hasRole("STUDENT")
+                        .requestMatchers(HttpMethod.GET, "/api/students/**").hasRole("STUDENT")
+                        .requestMatchers(HttpMethod.POST, "/api/students").hasRole("MENTOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/students").hasRole("MENTOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/students/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 }
